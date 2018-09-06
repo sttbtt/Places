7adae90f8ed868b94d7499e38302042f11be44e2
Scott Bennett <stt.btt@gmail.com>
2018-09-06 13:59:52 -0400

    Completed mapViewcontroller setup

--- Places/View Controllers/CreateNewPlacesViewController.swift
+++ Places/View Controllers/CreateNewPlacesViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class CreateNewPlacesViewController: UIViewController, PlacesPresenter {
     
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var latitudeTextField: UITextField!
+    @IBOutlet weak var longitudeTextField: UITextField!
+    
     var placeController: PlaceController?
 
     override func viewDidLoad() {
@@ -19,15 +23,19 @@ class CreateNewPlacesViewController: UIViewController, PlacesPresenter {
     }
 
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func createNewPlace(_ sender: Any) {
+        guard let name = nameTextField.text,
+            let latitudeString = latitudeTextField.text,
+            let latitude = Double(latitudeString),
+            let longitudeString = longitudeTextField.text,
+        let longitude = Double(longitudeString) else { return
+            
+            placeController?.creatPalce(with: name, latitude: latitude, longitude: longitude)
+        }
     }
-    */
+    
+    
+    
+
 
 }
--- Places/View Controllers/MapViewController.swift
+++ Places/View Controllers/MapViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var location: (latitude: Double, longitude: Double)? {
+        didSet {
+            updateMapViewLocation()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateMapViewLocation()
+        
+    }
+    
+    private func updateMapViewLocation() {
+        guard let location = location, isViewLoaded else { return }
+        
+        let span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        let coordinate = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        
+        mapView.setRegion(region, animated: true)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 
     /*
